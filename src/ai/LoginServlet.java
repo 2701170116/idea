203f76f82a60e84e066b0a0e04e5406fba0f0ceb
WSJ250640725 <250640725@qq.com>
2020-07-10 20:05:43 +0800

    登录界面小项目1

---
+++ src/ai/LoginServlet.java
@@ -0,0 +1,16 @@
+package ai;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginServlet extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
